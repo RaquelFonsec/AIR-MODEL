0f31c4abb187e51e320841e79c90afa06c2049c4
RaquelFonsec <raque-leto@hotmail.com>
2023-03-02 18:07:17 +0100

    criado controllers de flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,52 @@
-
 class FlatsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flats_params)
+    @flat.user = current_user
+    if @flat.save
+      redirect_to @flat, notice: "Flat criado com sucesso."
+    else
+     render 'new'
+      end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+   @flat =  Flat.find(params[:id])
+  end
+
+  def update
+  @flat =  Flat.find(params[:id])
+  if @flat.update(flats_params)
+    redirect_to @flat, notice: "Flat atualizado com sucesso."
+  else
+    render :edit
+  end
+  end
+
+  def destroy
+  @flat = Flat.find(params[:id])
+  @flat.destroy
+  redirect_to flats_path, notice: "Flat excluído com sucesso."
+  end
+
+    private
+
+  def flats_params
+params.require(:flat).permit(:city, :address, :price)
+
+   end
+ end
