306ea755c98075923d43dd92b092ee8337ae73e7
George Simei <georgesimei@gmail.com>
2023-03-03 19:37:57 +0100

    alteracoes em path navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "pages#home"
+  get "my_bookings", to: "pages#my_bookings"
+  get "my_flats", to: "pages#my_flats"
   resources :flats, except: :index do
     resources :bookings
     resources :comments
